ace9567ae6aad7bd3736c812a73b61dd8062d110
Jamie Bort <jamiebort@gmail.com>
2020-09-15 10:52:15 -0400

    Added a simple component.

--- trelloapi/src/App.js
+++ trelloapi/src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import TempComponent from './components/TempComponent';
+
 function App() {
+  console.log("I'm in.");
   return (
     <div className="App">
       <header className="App-header">
+      <TempComponent/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ trelloapi/src/components/TempComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class TempComponent extends React.Component {
+    render() {
+        console.log("I'm in2.");
+
+    
+    return <h1>HI</h1>;
+    }
+}
+
+export default TempComponent
