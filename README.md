aa069aafa76a5ea4ab459b4026305e1143e2139f
Jamie Bort <jamiebort@gmail.com>
2020-09-17 10:53:19 -0400

    The API is called when the component loads.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 1. ~~Generate a React project~~
 2. ~~Set up .env so that I can make api calls without compromising my key when I push files into git.~~
 3. Then make an api call.
-  a. have it load automatically when the component loads.
+  a. ~~have it load automatically when the component loads.~~
   b. then have it occur when I push a button (create a button)
   c. then make a button component in another file and pass the info via props
   d. then via state
--- trelloapi/src/App.js
+++ trelloapi/src/App.js
@@ -4,10 +4,19 @@ import './App.css';
 import TempComponent from './components/TempComponent';
 
 const REACT_APP_CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+const REACT_APP_KEY = process.env.REACT_APP_KEY;
+const REACT_APP_TOKEN = process.env.REACT_APP_TOKEN;
+// const missing = "oo";
 
 function App() {
-  console.log(process.env.REACT_APP_KEY);
-  console.log(process.env.REACT_APP_TOKEN);
+  console.log(REACT_APP_KEY);
+  console.log(REACT_APP_TOKEN);
+  // console.log(`https://www.go`+`ogle.com`);
+  // console.log(`https://www.g` + missing +`gle.com`);
+  // console.log(`https://api.trello.com/1/boards/59eb3c354ef89f2258c24900/cards?key=b2b04ca8f818a58b607c8e226d68003e&token=e3e3dccf8143f51db990f253506a49a226ff6ef7dfd8439c9c96d9818016451a`);
+  // console.log(`https://api.trello.com/1/boards/59eb3c354ef89f2258c24900/cards?key=` + `b2b04ca8f818a58b607c8e226d68003e&token=e3e3dccf8143f51db990f253506a49a226ff6ef7dfd8439c9c96d9818016451a`);
+  console.log(`https://api.trello.com/1/boards/59eb3c354ef89f2258c24900/cards?key=` + REACT_APP_KEY + `&token=` + REACT_APP_TOKEN);
+  // console.log(`https://api.trello.com/1/boards/59eb3c354ef89f2258c24900/cards?key={{key}}&token={{token}}`);
 
   return (
     <div className="App">
