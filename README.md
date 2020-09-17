081aa4f6e8d0297b6cc37bc461b026443fbb9027
Jamie Bort <jamiebort@gmail.com>
2020-09-17 10:25:20 -0400

    Got the dotenv process to work.

--- README.md
+++ README.md
@@ -6,3 +6,10 @@
 3. Then make an api call.
 4. then make the api call(s) I am interested in.
 5. Lastly create components for the api call.
+
+To fix the dependency tree, try following the steps below in the exact order:
+
+  1. Delete package-lock.json (not package.json!) and/or yarn.lock in your project folder.
+  2. Delete node_modules in your project folder.
+  3. Remove "webpack" from dependencies and/or devDependencies in the package.json file in your project folder.
+  4. Run npm install or yarn, depending on the package manager you use.
--- trelloapi/src/App.js
+++ trelloapi/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
 import TempComponent from './components/TempComponent';
 
+const REACT_APP_CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+
 function App() {
-  console.log("I'm in.");
+  // console.log(string);
+  console.log(process.env.REACT_APP_CLIENT_ID);
+
   return (
     <div className="App">
       <header className="App-header">
-      <TempComponent/>
+        <h1>{REACT_APP_CLIENT_ID}</h1>
+        <TempComponent />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
