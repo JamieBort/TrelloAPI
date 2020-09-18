51ad0c9142e22ee5a3331957cf6917a0ef0a3b81
Jamie Bort <jamiebort@gmail.com>
2020-09-18 10:16:08 -0400

    In trelloapi/src/App.js changed  to . So that I could add a constructor.

--- trelloapi/src/App.js
+++ trelloapi/src/App.js
@@ -7,14 +7,16 @@ const REACT_APP_KEY = process.env.REACT_APP_KEY;
 const REACT_APP_TOKEN = process.env.REACT_APP_TOKEN;
 const TrelloAPIstring = `https://api.trello.com/1/boards/59eb3c354ef89f2258c24900/cards?key=` + REACT_APP_KEY + `&token=` + REACT_APP_TOKEN;
 
-function App() {
+// function App() {
+  class App extends React.Component {
   // console.log(REACT_APP_KEY);
   // console.log(REACT_APP_TOKEN);
   // console.log(`https://api.trello.com/1/boards/59eb3c354ef89f2258c24900/cards?key=` + REACT_APP_KEY + `&token=` + REACT_APP_TOKEN);
 
+  render() {
   return (
     <div className="App">
-    <button onClick={apiCall}>Button Tag</button>
+    <button onClick={apiCall}>Button Tag. This makes an api call as well.</button>
     <APIButton push={() => console.log("Button Pushed")} apiFetch={apiCall} />
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -32,6 +34,7 @@ function App() {
       </header> */}
     </div>
   );
+    }
 }
 
 function apiCall() {
