04134bc0157da6ff845921fefa193da0dc47763f
Jamie Bort <jamiebort@gmail.com>
2020-10-08 13:48:35 -0400

    Commented out a line on this file because it's now broken. trelloapi/src/components/List.js.

--- trelloapi/src/components/List.js
+++ trelloapi/src/components/List.js
@@ -4,7 +4,8 @@ import Contact from './Contact';
 function List(props) {
     return (
         <div>
-            {props.details.map(c => <Contact key={c.id} name={c.name} email={c.email} phone={c.phone} />)}
+        {/* commented this out on account of adding the constructor and messing with props as a result */}
+            {/* {props.details.map(c => <Contact key={c.id} name={c.name} email={c.email} phone={c.phone} />)} */} 
         </div>
     );
 }
