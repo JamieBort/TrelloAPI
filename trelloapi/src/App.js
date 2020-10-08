1d0c03136afe62685f8152cf009476c6fb1b7e03
Jamie Bort <jamiebort@gmail.com>
2020-10-08 13:43:27 -0400

    Modified the trelloapi/src/App.js file for implementing a show/hide feature.

--- trelloapi/src/App.js
+++ trelloapi/src/App.js
@@ -11,21 +11,37 @@ const AllBoards = `https://api.trello.com/1/members/me/boards?key=` + process.en
 
 // function App() {
 class App extends React.Component {
-  state = {
+// currently missing constructor with state inside
+constructor(props){
+  super(props); // *** why is super crossed out? ***
+  this.state = {
     details: [],
   };
-  // componentDidMount() {console.log("this fires after the component mounts.")};
+  // this.handleClick = this.handleClick.bind(this); // or similar line. *** NOTE: this needs to change. ***
+}
 
-  // console.log(process.env.REACT_APP_KEY);
-  // console.log(process.env.REACT_APP_TOKEN);
-  // console.log(`https://api.trello.com/1/boards/59eb3c354ef89f2258c24900/cards?key=` + process.env.REACT_APP_KEY + `&token=` + process.env.REACT_APP_TOKEN);
-
-  // myList = (myData) => {
-  //   for (let i = 0; i < myData.length; i++) {
-  //     console.log(myData[i].name);
-  //   }
-  // }
-
+// also missing switch statement
+showHideComponentSwitchStatement(aVariable){
+  console.log(aVariable);
+  // console.log("hi");
+  // switch statement
+  let x = "x";
+  let y = "y";
+  let v = "v";
+  // let z;
+  let expression = v;
+  switch(expression) {
+    case x:
+      console.log("the expression is: ", expression);
+      break;
+    case y:
+      console.log("the expression is: ", expression);
+      break;
+    default:
+      console.log("the expression is: ", "z");
+  }
+}
+  
   apiCall = () => {
     axios
       .get(AllBoards)
@@ -50,62 +66,6 @@ class App extends React.Component {
       .catch(error => console.log(error));
 
   }
-
-  //   async apiCall = () => {
-  //     // async function getDataset() {
-  //       var response = await fetch(AllBoards)
-  //       // .then(response => console.log(response.json()))
-  //       // .then(response => {console.log(response.json());response.json();})
-  //       .then(response => response.json())
-  //       .then(data => {
-  //         console.log(data[0].name);
-  //         console.log(data);
-  //         // create an array of contacts only with relevant data
-  //         // const ABoard = data.map(c => {
-  //         //   return {
-  //         //     //     id: c.id,
-  //         //     name: c.name,
-  //         //     //     email: c.email,
-  //         //     //     phone: c.phone,
-  //         //   };
-  //         // });
-  //       })
-  //       .catch((error) => { console.error('Error:', error) });
-  //     // create an array of contacts only with relevant data
-  //     // const ABoard = data.map(c => {
-  //     //   return {
-  //     //     //     id: c.id,
-  //     //     name: c.name,
-  //     //     //     email: c.email,
-  //     //     //     phone: c.phone,
-  //     //   };
-  //     // });
-
-  //     console.log(response.json()); // this line will "wait" for the previous to be completed
-  // //     console.log(getDataset)
-  // // }
-  //   };
-
-  // apiCallAllBoards = () => {
-  //   fetch(AllBoards)
-  //     .then(response => response.json())
-  //     .then(data => console.log(data))
-  //     // .then(data => this.setState({ apiName: data, }))
-  //     // .then(
-  //     //   this.setState({
-  //     //     apiName: {
-  //     //       ...this.state.apiName,this.data
-  //     //     }
-  //     //   })
-  //     // )
-  //     .then(console.log("The new state", this.state))
-  //     // .then(this.setState({...this.state.apiInfo, apiInfo: this.data}))     
-  //     // .then(()=>this.setState({ apiInfo: this.data, }))
-  //     // .then(console.log(this.state.apiInfo))
-  //     // .then(console.log(data[0].name))
-  //     // .then(this.setState({ apiName: this.data[0].name, }))
-  //     .catch((error) => { console.error('Error:', error) });
-  // };
 
   updateState = () => {
     console.log("Updated state.");
@@ -139,13 +99,16 @@ class App extends React.Component {
   //     .catch((error) => { console.error('Error:', error) });
   // };
 
-  testFunction = () => { console.log(this.state) };
+  currentStateFunction = () => { console.log("The current state is:"); console.log(this.state); };
 
   render() {
     return (
       <div className="App">
         <div className="section">
-          <APIButtonWithProps message="The current state." theFunction={this.testFunction}></APIButtonWithProps>
+          <APIButtonWithProps message="Show/Hide Switch Button." theFunction={this.showHideComponentSwitchStatement}></APIButtonWithProps>
+        </div>
+        <div className="section">
+          <APIButtonWithProps message="The current state." theFunction={this.currentStateFunction}></APIButtonWithProps>
         </div>
         <div className="section">
           <APIButtonWithProps message="List the boards" theFunction={this.multifunctionButton} />
